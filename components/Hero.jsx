222a838a77e8eb4abab5b182372aaf5c8cb53753
victorocna <victorocnarescu@gmail.com>
2021-03-06 13:47:35 +0200

    update hero props

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const Hero = () => {
+const Hero = ({ minHeight = 400, children }) => {
   return (
-    <div className="hero min-h-screen text-white p-4">
-      <h2 className="text-4xl">Hero image</h2>
-      <p className="text-lg">
-        Capture the attention of your audience with this hero image. Less is more
-      </p>
+    <div className="hero text-white p-4" style={{ minHeight }}>
+      {children}
     </div>
   );
 };
